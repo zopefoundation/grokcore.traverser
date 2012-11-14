cc4905dbc611cf5ef2e4c34c51223bebc6198c11
Christian Klinger <cklinger@novareto.de>
2012-11-14 08:37:38 +0000

    Preparing release 1.2

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ tests_require = [
 
 setup(
     name='grokcore.traverser',
-    version='1.2.dev0',
+    version='1.2',
     author='Grok Team',
     author_email='grok-dev@zope.org',
     url='http://grok.zope.org',
